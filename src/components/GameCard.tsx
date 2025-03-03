d75a9303cb9c70cbe23f1561136d5c669081f49c
Robin <robin@canvasgroup.com.au>
2025-03-03 21:43:51 +1100

    get Crop image url

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, Image, Heading, HStack } from "@chakra-ui/react";
 import { Game, Platform } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CreditScore from "./CreditScore";
+import getCroppedImageUrl from "@/services/image-url";
 
 interface Pros {
   game: Game;
@@ -10,7 +11,7 @@ interface Pros {
 const GameCard = ({ game }: Pros) => {
   return (
     <Card.Root borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justify="space-between">

5e22ad36c9bedcbabaf2da57993fadaddd0291cf
Robin <lizheng.robin@gmail.com>
2025-03-19 17:26:27 +1100

    refactor entities

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,5 @@
 import { Card, CardBody, Image, Heading, HStack } from "@chakra-ui/react";
-import { Game } from "../hooks/useGames";
+import { Game } from "../entities/Game";
 import PlatformIconList from "./PlatformIconList";
 import CreditScore from "./CreditScore";
 import getCroppedImageUrl from "@/services/image-url";
